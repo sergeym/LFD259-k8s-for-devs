4101f292f4d70491aeb2ad7f0b1b8f0c54de9af2
Sergey Marin <marin.sergey@gmail.com>
2022-02-10 13:56:47 +0200

    lab 3.1

---
+++ Labs/lab-3.1-deploy-app/app1/simple.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import time
+import socket
+
+while True:
+    host = socket.gethostname()
+    date = time.strftime("%Y-%m-%d%H:%M:%S")
+    now = str(date)
+    f = open("date.out","a" )
+    f.write(now +"\n")
+    f.write(host +"\n")
+    f.close()
+
+    print("1")
+
+    time.sleep(5)
